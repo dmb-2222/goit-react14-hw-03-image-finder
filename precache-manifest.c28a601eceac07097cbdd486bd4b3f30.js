604a817a3907aba6e2e829b5a2f9ddd2e537c011
dmb-2222 <dmb-2222@yandex.ru>
2019-11-06 13:04:38 +0200

    Updates

--- precache-manifest.c28a601eceac07097cbdd486bd4b3f30.js
+++ precache-manifest.c28a601eceac07097cbdd486bd4b3f30.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "34408f22f66485311af7caa06c6a9718",
+    "revision": "1077c80028f113c097e08b08767e4ae1",
     "url": "/goit-react14-hw-03-image-finder/index.html"
   },
   {
-    "revision": "c9215134cd0d20721cc3",
-    "url": "/goit-react14-hw-03-image-finder/static/css/main.504c9bf7.chunk.css"
+    "revision": "7d3aeb043b5268bef72c",
+    "url": "/goit-react14-hw-03-image-finder/static/css/main.5e053ce6.chunk.css"
   },
   {
     "revision": "bd9b7ab5cc8ee752c38a",
     "url": "/goit-react14-hw-03-image-finder/static/js/2.8ca467ce.chunk.js"
   },
   {
-    "revision": "c9215134cd0d20721cc3",
-    "url": "/goit-react14-hw-03-image-finder/static/js/main.02118ed0.chunk.js"
+    "revision": "7d3aeb043b5268bef72c",
+    "url": "/goit-react14-hw-03-image-finder/static/js/main.e4a7c984.chunk.js"
   },
   {
     "revision": "c7183dd988202a1df8e0",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react14-hw-03-image-finder/precache-manifest.6bbf0536508a5f5a90cae0dd7d9ee251.js"
+  "/goit-react14-hw-03-image-finder/precache-manifest.c28a601eceac07097cbdd486bd4b3f30.js"
 );
 
 self.addEventListener('message', (event) => {
